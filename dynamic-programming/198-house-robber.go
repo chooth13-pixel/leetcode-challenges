c4028c56df3c67e92b2e54a24228e3ded13a90e7
tomc <chooth13@gmail.com>
2026-07-15 15:31:58 +0800

    refactor code

--- dynamic-programming/198-house-robber.go
+++ dynamic-programming/198-house-robber.go
@@ -5,9 +5,10 @@
 // Time O(n) Space O(1)
 
 func rob(nums []int) int {
-	dp := []int{0, nums[0]}
+	dp1, dp2 := 0, nums[0]
+
 	for i := 1; i < len(nums); i++ {
-		dp = []int{dp[1], int(math.Max(float64(dp[0]+nums[i]), float64(dp[1])))}
+		dp1, dp2 = dp2, max(dp1+nums[i], dp2)
 	}
-	return dp[1]
+	return dp2
 }
